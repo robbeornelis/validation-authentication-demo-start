32ec833036b9d17325c6700586721b11139baf7a
Frederick Roegiers <frederick@rogerthat.be>
2024-03-12 09:02:29 +0100

    wip

--- src/lib/MailTransporter.js
+++ src/lib/MailTransporter.js
@@ -3,11 +3,11 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const transporter = nodemailer.createTransport({
-  host: process.env.MAIL_HOST,
-  port: process.env.MAIL_PORT,
+  host: process.env.MAIL_HOST || "localhost",
+  port: process.env.MAIL_PORT || 2025,
   auth: {
-    user: process.env.MAIL_USER,
-    pass: process.env.MAIL_PASS,
+    user: process.env.MAIL_USER || "project.2",
+    pass: process.env.MAIL_PASS || "secret.2",
   },
 });
 
