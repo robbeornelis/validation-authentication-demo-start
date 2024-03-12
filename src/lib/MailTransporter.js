c6363a094b4a1717c2ac16d209920b4987fbed4b
Frederick Roegiers <frederick@rogerthat.be>
2024-03-12 10:33:19 +0100

    wip

--- src/lib/MailTransporter.js
+++ src/lib/MailTransporter.js
@@ -2,6 +2,10 @@ import nodemailer from "nodemailer";
 import dotenv from "dotenv";
 dotenv.config();
 
+import hbs from "nodemailer-express-handlebars";
+import path from "path";
+import { VIEWS_PATH } from "../consts.js";
+
 const transporter = nodemailer.createTransport({
   host: process.env.MAIL_HOST || "localhost",
   port: process.env.MAIL_PORT || 1025,
@@ -10,5 +14,19 @@ const transporter = nodemailer.createTransport({
     pass: process.env.MAIL_PASS || "secret.1",
   },
 });
+
+transporter.use(
+  "compile",
+  hbs({
+    viewEngine: {
+      extName: ".hbs",
+      partialsDir: path.join(VIEWS_PATH, "partials"),
+      layoutsDir: path.join(VIEWS_PATH, "layouts"),
+      defaultLayout: "mail.hbs",
+    },
+    viewPath: path.join(VIEWS_PATH, "emails"),
+    extName: ".hbs",
+  })
+);
 
 export default transporter;
